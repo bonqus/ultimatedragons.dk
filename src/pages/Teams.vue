d4b0d7011ef383984135bcfce17328de9113ec78
Mathias Bohn Rasmussen <mathias_b2@hotmail.com>
2021-06-03 09:36:13 +0200

    maps

--- src/pages/Teams.vue
+++ src/pages/Teams.vue
@@ -5,44 +5,44 @@
         <div class="q-px-md q-pb-xl offset-md-4 col-md-4 offset-sm-2 col-sm-8 col-xs">
           <h2 class="q-mb-md">{{ $t("Open") }}</h2>
           <h3 class="q-mb-md q-mt-none">{{ $t("Practice") }}</h3>
-          <q-markup-table flat bordered separator="none">
+          <q-markup-table flat bordered separator="none" class="cursor-pointer">
             <tbody>
-              <tr @click="map = true">
+              <tr @click="mapf = true">
                 <td class="text-left">Fælledparken</td>
                 <td class="text-left">{{ $t("Monday") }}</td>
-                <td class="text-right">17:00 - 19:00</td>
+                <td class="text-right">17:30 - 19:30</td>
               </tr>
-              <tr @click="map = true">
-                <td class="text-left">Fælledparken</td>
+              <tr @click="mapl = true">
+                <td class="text-left">Lundehusparken</td>
                 <td class="text-left">{{ $t("Thursday") }}</td>
-                <td class="text-right">17:00 - 19:00</td>
+                <td class="text-right">17:30 - 19:30</td>
               </tr>
             </tbody>
           </q-markup-table>
 
           <h2 class="q-mb-md">{{ $t("Women") }}</h2>
           <h3 class="q-mb-md q-mt-none">{{ $t("Practice") }}</h3>
-          <q-markup-table flat bordered separator="none">
+          <q-markup-table flat bordered separator="none" class="cursor-pointer">
             <tbody>
-              <tr @click="map = true">
+              <tr @click="mapf = true">
                 <td class="text-left">Fælledparken</td>
                 <td class="text-left">{{ $t("Tuesday") }}</td>
                 <td class="text-right">17:00 - 19:00</td>
               </tr>
-              <tr @click="map = true">
-                <td class="text-left">Fælledparken</td>
+              <tr @click="mapl = true">
+                <td class="text-left">Lundehusparken</td>
                 <td class="text-left">{{ $t("Thursday") }}</td>
-                <td class="text-right">17:00 - 19:00</td>
+                <td class="text-right">17:30 - 19:30</td>
               </tr>
             </tbody>
           </q-markup-table>
 
           <h2 class="q-mb-md">{{ $t("Mixed") }}</h2>
           <h3 class="q-mb-md q-mt-none">{{ $t("Practice") }}</h3>
-          <q-markup-table flat bordered separator="none">
+          <q-markup-table flat bordered separator="none" class="cursor-pointer">
             <tbody>
-              <tr @click="map = true">
-                <td class="text-left">Fælledparken</td>
+              <tr @click="mapl = true">
+                <td class="text-left">Lundehusparken</td>
                 <td class="text-left">{{ $t("Thursday") }}</td>
                 <td class="text-right">17:30 - 19:30</td>
               </tr>
@@ -51,7 +51,38 @@
         </div>
       </div>
     </q-scroll-area>
-    <q-dialog v-model="map">
+    <q-dialog v-model="mapl">
+      <q-card
+        class="column"
+        style="
+        height: 500px;
+        width: 500px;
+        "
+      >
+        <q-card-section
+          class="col q-pa-none"
+          style="
+          -webkit-filter: grayscale(100%);
+          filter: grayscale(100%);"
+        >
+          <iframe
+            width="100%"
+            height="100%"
+            frameborder="0"
+            scrolling="no"
+            style="border:0"
+            marginheight="0"
+            marginwidth="0"
+            src="https://www.google.com/maps/embed/v1/place?q=55.717213%2C%2012.548236&key=AIzaSyANaCyizbUBRWXlKKzgpyinlHjG9a8T2gQ"
+            allowfullscreen
+          ></iframe>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat :label="$t('OK')" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <q-dialog v-model="mapf">
       <q-card
         class="column"
         style="
@@ -89,7 +120,8 @@ export default {
   name: "TeamsPage",
   data() {
     return {
-      map: false
+      mapf: false,
+      mapl: false
     };
   }
 };
