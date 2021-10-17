980f5afecf33805f60a4ff1f78e83a170ad834bd
Mathias Bohn Rasmussen <mathias_b2@hotmail.com>
2021-10-17 22:56:16 +0200

    order of gallery and contact

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -74,8 +74,8 @@ export default defineComponent({
       links: [
         { icon: "group", label: "Practice", to: "/practice" },
         { icon: "store", label: "Shop", to: "/shop" },
-        { icon: "alternate_email", label: "Contact", to: "/contact" },
-        { icon: "collections", label: "Gallery", to: "/gallery" }
+        { icon: "collections", label: "Gallery", to: "/gallery" },
+        { icon: "alternate_email", label: "Contact", to: "/contact" }
       ]
     };
   },
